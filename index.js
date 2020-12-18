5817fb03a2f2f302daf0283c0635105c2b16c1d5
Shane Claussen <sclaussen@yahoo.com>
2020-12-18 15:21:38 -0800

    Completed day 1 part 2 problem - n+n+n=2020

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ main(process.argv);
 
 
 function main(args) {
-    day1();
+    day1a();
+    day1b();
 }
 
 
-function day1() {
+function day1a() {
+    console.log('\nDay 1 Part 1');
+
     let data = fs.readFileSync('./input.day1', 'utf8')
     let lines = data.split(/\r?\n/);
 
@@ -31,7 +34,37 @@ function day1() {
             if (indexedLines[i] + indexedLines[j] === 2020) {
                 console.log(indexedLines[i] + ' + ' + indexedLines[j] + ' = ' + (indexedLines[i] + indexedLines[j]));
                 console.log(indexedLines[i] + ' * ' + indexedLines[j] + ' = ' + (indexedLines[i] * indexedLines[j]));
-                process.exit(0);
+                return;
+            }
+        }
+    }
+}
+
+
+function day1b() {
+    console.log('\nDay 1 Part 2');
+    let data = fs.readFileSync('./input.day1', 'utf8')
+    let lines = data.split(/\r?\n/);
+
+    let indexedLines = {};
+    let i = 0;
+    for (let line of lines) {
+        indexedLines[i] = parseInt(line);
+        i++;
+    }
+
+    for (let i = 0; i < Object.keys(indexedLines).length; i++) {
+        for (let j = i + 1; j < Object.keys(indexedLines).length; j++) {
+            for (let k = 0; k < Object.keys(indexedLines).length; k++) {
+                if (k === i || k === j) {
+                    continue;
+                }
+
+                if (indexedLines[i] + indexedLines[j] + indexedLines[k] === 2020) {
+                    console.log(indexedLines[i] + ' + ' + indexedLines[j] + ' + ' + indexedLines[k] + ' = ' + (indexedLines[i] + indexedLines[j] + indexedLines[k]));
+                    console.log(indexedLines[i] + ' * ' + indexedLines[j] + ' * ' + indexedLines[k] + ' = ' + (indexedLines[i] * indexedLines[j] * indexedLines[k]));
+                    return;
+                }
             }
         }
     }
